271ab03d67da7f8a1220f170b9309b624c2d9f87
braiandaros <braianjunior666@gmail.com>
2024-08-24 22:05:18 -0300

    igualdade de strings

---
+++ src/fundamentos/IgualdadeString.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class IgualdadeString {
+	public static void main(String[] args) {
+		System.out.println("2" == "2");
+		String s = new String("2");
+		System.out.println("2" == s);
+		System.out.println("2".equals(s));
+		
+	}
+}
---
+++ src/fundamentos/OperadoresTernarios.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class OperadoresTernarios {
+	public static void main(String[] args) {
+		double media = 5.6;
+		String resultado = media >= 7.0 ? "Aprovado" : "Reprovado";
+		
+		System.out.println(resultado);
+	}
+}
