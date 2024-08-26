e2c4fef1dfe4c380a17361ccc427f0c108d48f5c
braiandaros <braianjunior666@gmail.com>
2024-08-26 16:49:51 -0300

    if

--- src/EstruturasDeControle/If.java
+++ src/EstruturasDeControle/If.java
@@ -1,5 +1,18 @@
 package EstruturasDeControle;
 
-public class If {
+import java.util.Scanner;
 
+public class If {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.print("Digite a nota do aluno: ");
+		double media = entrada.nextDouble();
+		
+		if(media >= 7) {
+			System.out.println("Aprovado");
+			System.out.println("Parabens");
+		}
+		
+		entrada.close();
+	}
 }
