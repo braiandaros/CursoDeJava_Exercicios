f4c998bda2926a4f1b1551e469d2cc5ec4dc3d38
braiandaros <braianjunior666@gmail.com>
2024-08-31 15:36:18 -0300

    while

---
+++ src/EstruturasDeControle/While.java
@@ -0,0 +1,11 @@
+package EstruturasDeControle;
+
+public class While {
+	public static void main(String[] args) {
+	int c = 1;
+	while (c <= 10) {
+		System.out.printf("ola pessoa: " + c + "\n");
+		c++;
+	}
+	}
+}
