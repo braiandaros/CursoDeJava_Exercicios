a0e20ed74f3bf1cf76c5b1180f001bc3950fd94b
braiandaros <braianjunior666@gmail.com>
2024-08-31 15:44:34 -0300

    for

---
+++ src/EstruturasDeControle/For1.java
@@ -0,0 +1,9 @@
+package EstruturasDeControle;
+
+public class For1 {
+	public static void main(String[] args) {
+		for(int c = 1; c<=10; c++) {
+			System.out.printf("Ola pessoa: " + c + "\n");
+		}
+	}
+}
