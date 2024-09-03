acbbc10727b790c1c440c221a6bb1df3905858e1
braiandaros <braianjunior666@gmail.com>
2024-09-03 17:27:23 -0300

    desafio

--- src/EstruturasDeControle/DesafioDoWhile.java
+++ src/EstruturasDeControle/DesafioDoWhile.java
@@ -4,21 +4,28 @@ import java.util.Scanner;
 
 public class DesafioDoWhile {
 	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
 		float nota, total = 0;
 		int cont = 0;
 		do {
 			System.out.print("Digite a nota: ");
-			Scanner entrada = new Scanner(System.in);
+			
 			nota = entrada.nextFloat();
+			
 			
 			if(nota <= 10 && nota >= 0) {
 				total = total + nota;
 				cont++;
 			}
+			else if(nota == -1) {
+				System.out.println("Codigo encerrado.");
+			}
 			else {
 				System.out.println("Digite uma nota valida.");
 			}	
 		}while(nota != -1);
-		System.out.println("O total das notas foram de " + total + " e o numero de notas digitadas foram de " + cont);
+		entrada.close();
+		System.out.println("O total das notas foram de " + total + " e o numero de notas validas digitadas foram de " + cont);
 	}
+	
 }
