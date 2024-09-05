691a06006f0366a520ad65e92534050421bb0a8a
braiandaros <braianjunior666@gmail.com>
2024-09-05 15:41:47 -0300

    for

---
+++ src/EstruturasDeControle/For2.java
@@ -0,0 +1,9 @@
+package EstruturasDeControle;
+
+public class For2 {
+	public static void main(String[] args) {
+		for(int i = 1000; i >= 0; i-=5 ) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/EstruturasDeControle/For3.java
@@ -0,0 +1,10 @@
+package EstruturasDeControle;
+
+public class For3 {
+	public static void main(String[] args) {
+		int x;
+		for(x = 0; x < 10; x++) {
+			
+		}
+	}
+}
