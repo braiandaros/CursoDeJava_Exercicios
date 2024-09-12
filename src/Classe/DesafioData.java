09d91ae5600f9ef4a164acc754c619c692253549
braiandaros <braianjunior666@gmail.com>
2024-09-12 16:16:19 -0300

    desafio construtores

--- src/Classe/DesafioData.java
+++ src/Classe/DesafioData.java
@@ -4,7 +4,20 @@ public class DesafioData {
 	int dia;
 	int mes;
 	int ano;
+	int diaInicial = 1;
+	int mesInicial = 1;
+	int anoInicial = 1970;
 	
+	DesafioData(){
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+	DesafioData(int diaInicial, int mesInicial,int anoInicial){
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
 	String obterDataFormatada(){	
 		return String.format("%d/%d/%d", dia, mes, ano);
 	}
--- src/Classe/DesafioDataTeste.java
+++ src/Classe/DesafioDataTeste.java
@@ -7,16 +7,17 @@ public class DesafioDataTeste {
 		d1.mes = 01;
 		d1.ano = 2005;
 		
-		DesafioData d2 = new DesafioData();
-		d2.dia = 20;
-		d2.mes = 10;
-		d2.ano = 2075;
+		DesafioData d2 = new DesafioData(20, 10, 2075);
 		
-		System.out.println(d1.dia + "/" + d1.mes + "/" + d1.ano);
-		System.out.println(d2.dia + "/" + d2.mes + "/" + d2.ano);
+		DesafioData d3 = new DesafioData();
+
+		//System.out.println(d1.dia + "/" + d1.mes + "/" + d1.ano);
+		//System.out.println(d2.dia + "/" + d2.mes + "/" + d2.ano);
+		//System.out.println(d3.dia + "/" + d3.mes + "/" + d3.ano);
 		
 		System.out.println(d1.obterDataFormatada());
 		System.out.println(d2.obterDataFormatada());
+		System.out.println(d3.obterDataFormatada());
 
 	}
 }
