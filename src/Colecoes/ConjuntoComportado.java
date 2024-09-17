37b709f8bd82472e36959c57fddcf0eb2ba137a0
braiandaros <braianjunior666@gmail.com>
2024-09-17 15:53:41 -0300

    conjuntos

---
+++ src/Colecoes/ConjuntoComportado.java
@@ -0,0 +1,19 @@
+package Colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+	public static void main(String[] args) {
+		Set<String> lista = new HashSet<>();
+		//SortedSet<String> lista = new TreeSet<>();
+		lista.add("batata");
+		lista.add("banana");
+		lista.add("uva");
+		lista.add("maca");
+		
+		for(String candidatos: lista) {
+			System.out.println(candidatos);
+		}
+	}}
