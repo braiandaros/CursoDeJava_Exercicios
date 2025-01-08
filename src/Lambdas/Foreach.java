7ece43c1937820fc60c935e730d82f0484036a86
braian <braianjunior666@gmail.com>
2025-01-07 21:07:25 -0300

    lambdas

---
+++ src/Lambdas/Foreach.java
@@ -0,0 +1,30 @@
+package Lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Foreach {
+	public static void main(String[] args) {
+		List<String> aprovados = Arrays.asList("Ana", "Bia", "Lia", "Gui");
+		
+		System.out.println("forma normal");
+		for(String nome: aprovados) {
+			System.out.println(nome);
+		}
+		
+		System.out.println("\nLambda");
+		aprovados.forEach(nome -> {System.out.println(nome);});
+		
+		
+		System.out.println("\nMethod reference");
+		aprovados.forEach(System.out::println);
+		
+		
+		System.out.println("\nLambda 2");
+		aprovados.forEach(nome -> meuImprimir(nome));
+		
+	}	
+	static void meuImprimir(String nome) {
+		System.out.println("oi, meu nome é " + nome);
+	}
+}
